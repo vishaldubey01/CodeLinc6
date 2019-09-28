6f154fe07409365377c78dd1373fbc8dd62b36d3
Harish Yerra <harish.yerra@duke.edu>
2019-09-28 17:06:03 -0400

    Create initial implementation of dashboard

--- ConnectCorps/User Interface/ContentView.swift
+++ ConnectCorps/User Interface/ContentView.swift
@@ -10,12 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            DashboardView()
+                .navigationBarTitle(Text("Dashboard"))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
---
+++ ConnectCorps/User Interface/DashboardView.swift
@@ -0,0 +1,23 @@
+//
+//  DashboardView.swift
+//  ConnectCorps
+//
+//  Created by Harish Yerra on 9/28/19.
+//  Copyright © 2019 Harish Yerra. All rights reserved.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        VStack {
+            CCTabView()
+        }
+    }
+}
+
+struct DashboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardView()
+    }
+}
