028aa104a9403623b2827b9ede62388e006fce80
Harish Yerra <harish.yerra@duke.edu>
2019-09-28 15:34:35 -0400

    Initial Commit

---
+++ ConnectCorps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConnectCorps
+//
+//  Created by Harish Yerra on 9/28/19.
+//  Copyright © 2019 Harish Yerra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
